a32db8720dac83ced31c03c35f1c37ba0a9541dd
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2025-10-18 17:09:49 +0100

    Organised default homepage into folders

---
+++ src/app/components/myui/Header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Header() {
+	return (
+		<header className="">
+			<Image
+				className="dark:invert"
+				src="/next.svg"
+				alt="Next.js logo"
+				width={180}
+				height={38}
+				priority
+			/>
+		</header>
+	);
+}
