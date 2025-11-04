906e879349dabcd17f6f46315c7e74b7408f85c5
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2025-11-04 11:51:42 +0000

    Update Header content

--- src/app/components/myui/Header.tsx
+++ src/app/components/myui/Header.tsx
@@ -1,16 +1,20 @@
-import Image from "next/image";
+import { MapPin } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 export default function Header() {
 	return (
-		<header className="">
-			<Image
-				className="dark:invert"
-				src="/next.svg"
-				alt="Next.js logo"
-				width={180}
-				height={38}
-				priority
-			/>
+		<header className="m-4">
+			<div className="flex items-center gap-3 mb-2">
+				<MapPin className="h-8 w-8 text-primary" />
+				<h1 className="text-3xl font-bold text-balance">
+					UK Location Data Explorer
+				</h1>
+			</div>
+			<p className="text-muted-foreground text-pretty">
+				Use the search area below to search for a UK postcode and view data for
+				that postcode!
+			</p>
+			<Separator />
 		</header>
 	);
 }
