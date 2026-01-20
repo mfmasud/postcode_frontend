212de4d8fb98e807cea90b9a2fdf2fe2b2a5af23
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2026-01-20 17:18:20 +0000

    Add link to status page into the footer
    Also made the link to the source code open in a new page and added underline hover effects

--- src/components/myui/Footer.tsx
+++ src/components/myui/Footer.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
+
 export default function Footer() {
 	return (
 		<footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
 			<p>
 				UK Location Data Explorer |{" "}
-				<a href="https://github.com/mfmasud/postcode_frontend">Source</a>
+				<Link href="/status" className="hover:underline">
+					Status
+				</Link>{" "}
+				|{" "}
+				<a
+					href="https://github.com/mfmasud/postcode_frontend"
+					className="hover:underline"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Source
+				</a>
 			</p>
 		</footer>
 	);
